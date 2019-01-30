b68ae3c44ecaf5cd6d256d5b9df4eaa0a01637b4
dan2014 <dmen326@gmail.com>
2019-01-30 18:31:41 -0500

    Completed MVP

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,23 @@
-// Test away!
+import React from 'react';
+import {render, cleanup} from 'react-testing-library';
+import Display from './Display';
+import 'jest-dom/extend-expect'
+
+afterEach(cleanup)
+
+describe("<Display />", () => {
+    describe('Display Component State Open-Closed', () => {
+        it("State should be open at start", () => {
+            const {getByText} = render(<Display/>);
+            const openClosedState = getByText(/open/i);
+            expect(openClosedState.textContent).toBe("Open");
+        })
+    })
+    describe('Display Component State Unlocked-Locked', () => {
+        it("State should be unlocked at start", () => {
+            const {getByText} = render(<Display/>);
+            const unlockedLockedState = getByText(/unlocked/i);
+            expect(unlockedLockedState.textContent).toBe("Unlocked");
+        })
+    })
+})
